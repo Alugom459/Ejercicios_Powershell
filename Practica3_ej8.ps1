9acb4d9ab45747b7baf8ab545224b2b1f58c1518
Alugom459 <75848571+Alugom459@users.noreply.github.com>
2023-10-01 21:07:34 +0200

    Delete Practica3_ej8.ps1

--- Practica3_ej8.ps1
+++
@@ -1,6 +0,0 @@
-$nombre = read-host "Cual es su nombre?"
-$sexo = read-host "Cual es su sexo?"
-
-if ($sexo -eq "Mujer" -and $nombre -le "M") { Write-host "Perteneces al grupo A" }
-Elseif ($sexo -eq "Hombre" -and $nombre -ge "N" ) { Write-host "Perteneces al grupo A" }
-else { Write-host "Perteneces al grupo B"}
