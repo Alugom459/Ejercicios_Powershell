00c9a6f4212a8527ac7bf498361c41c3348361d0
Alugom459 <75848571+Alugom459@users.noreply.github.com>
2023-10-01 21:05:28 +0200

    Add files via upload

---
+++ Practica3/Practica3_ej10.ps1
@@ -0,0 +1,13 @@
+﻿[int]$puntos = Read-Host "Introduzca los puntos obtenidos"
+
+Switch ($puntos) {
+
+{ ($puntos -eq 0.0) } {Write-host "Inaceptable"}
+
+{ ($puntos -eq 0.4) } {Write-host "Aceptable"}
+
+{ ($puntos -eq 0.6) } {Write-host "Meritorio"}
+
+}
+
+Write-host "Le corresponde" ($puntos * 2400)
---
+++ Practica3/Practica3_ej11.ps1
@@ -0,0 +1,9 @@
+﻿$edad = read-host "Introduce la edad"
+
+if ($edad -lt 4) {
+    Write-host "La entrada es gratuita"
+} elseif ($edad -ge 4 -and $edad -le 18) {
+   Write-host "El precio es de 5€"
+} else {
+   Write-host "El precio es de 10€"
+}
