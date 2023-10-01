cfabf0662c6b9fb8b21a7e29e6c2663255eb8c95
Alugom459 <75848571+Alugom459@users.noreply.github.com>
2023-10-01 21:07:27 +0200

    Delete Practica3_ej7.ps1

--- Practica3_ej7.ps1
+++
@@ -1,7 +0,0 @@
-
-[string]$guess = Read-host "Escriba la contraseña:"
-[string]$contraseña = "c0ntr4seña"
-
-If ($guess -eq $contraseña) { Write-host "La contraseña es correcta"}
-Else {Write-host "la contraseña es incorrecta"}
-
