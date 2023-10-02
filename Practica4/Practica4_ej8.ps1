fb71eecac3fcdcd865acef2168a3aff0ab9f237c
Alugom459 <75848571+Alugom459@users.noreply.github.com>
2023-10-02 08:30:04 +0200

    Update Practica4_ej8.ps1

--- Practica4/Practica4_ej8.ps1
+++ Practica4/Practica4_ej8.ps1
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 [int]$num = Read-Host "Introduzca el numero?"
 
 for ( $i=1; $i -le $num; $i++ ) {
@@ -8,16 +8,3 @@ Write-Host ""$esc=$i*2-1
     write-host -NoNewline "$esc "$esc -=2}
     }
     
-=======
-$num = Read-host "Introduce un numero entero"
-
-
-for ($i = 1; $i -le $num; $i++) { 
-    Write-Host 
-    for ($j = $i; $j -ge 1; $j--)
-    { 
-        write-host -nonewline "$j " 
-    } 
-}
-
->>>>>>> d988c7b (.)
