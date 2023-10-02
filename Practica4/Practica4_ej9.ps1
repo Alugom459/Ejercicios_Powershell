1860edbd85f3c77d59a154de90a56e8557d68f5e
Alugom459 <75848571+Alugom459@users.noreply.github.com>
2023-10-02 08:29:28 +0200

    Update Practica4_ej9.ps1

--- Practica4/Practica4_ej9.ps1
+++ Practica4/Practica4_ej9.ps1
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $contraseña = "C0ntraseña"
 
 $intento = Read-Host "Ingrese la contraseña"
@@ -6,16 +6,5 @@ $intento = Read-Host "Ingrese la contraseña"
 while ($intento -ne $Contraseña) {
     Write-Host "Contraseña incorrecta. Inténtelo de nuevo."
     $intento = Read-Host "Ingrese la contraseña"
-=======
-[string]$pass = Read-host "Introduzca la contrase馻"
+}
 
-while ($pass -ne [string]"Contrase馻") {
-
-    if ($pass -eq [string]"Contrase馻") { Write-host "La contrase馻 es correcta"}
-    else {Write-host "La contrase馻 es incorrecta"
-        $pass = Read-host "Introduzca la contrase馻"
-    }
-  
->>>>>>> d988c7b (.)
-
-    }
