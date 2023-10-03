abcdb714f06074fe59e3a4de083fa35dd9b3a878
Juanma Gomez <alugom459@ieselcaminas.org>
2023-10-03 10:13:18 +0200

    Añadiendo la Practica5

---
+++ Practica5/Practica5_ej1.ps1
@@ -0,0 +1,65 @@
+﻿
+
+Function Sumar {
+    param (
+        [int]$x,
+        [int]$y
+    )
+    $sumar = $x + $y
+    Write-Host "La respuesta es $sumar"
+}
+
+Function Restar {
+    param (
+        [int]$x,
+        [int]$y
+    )
+    $Restar = $x - $y
+    Write-Host "La respuesta es $Restar"
+}
+
+Function Multiplicar {
+    param (
+        [int]$x,
+        [int]$y
+    )
+    $sumar = $x * $y
+    Write-Host "La respuesta es $Multiplicar"
+
+
+}
+
+Function Resto {
+    param (
+        [int]$x,
+        [int]$y
+    )
+    $Resto = $x % $y
+    Write-Host "La respuesta es $Resto"
+}
+
+
+Write-Host "**** CALCULADORA ****"
+Write-Host "1.Sumar"
+Write-host "2.Restar"
+Write-host "3.Multiplicar"
+Write-host "4.Resto"
+
+$Op = Read-Host "¿Qué desea hacer? Elige una opción:"
+
+[int]$x = Read-host "Introduce el primer numero"
+[int]$y = Read-host "Introduce el segundo numero"
+
+switch ($op) { 
+
+1 { Sumar $x $y }
+
+2 { Resta $x $y }
+
+3 { Multiplicar $x $y }
+
+4 { Resto $x $y }
+
+default { Write-host "No es ninguna operación" }
+
+}
---
+++ Practica5/Practica5_ej2.ps1
@@ -0,0 +1,6 @@
+﻿$usuarios = Import-Csv Z:\Practica5\usuarios.csv
+
+foreach ($usu in $Usuarios)
+{
+ 	Write-Host "Usuario: $($usu.nombre) $($usu.apellidos) $($usu.grupo)"
+}
