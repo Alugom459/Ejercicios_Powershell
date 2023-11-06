dc003dcd89bddb4ac5d398326673ecdf8add17e7
Juanma Gomez <alugom459@ieselcaminas.org>
2023-11-06 10:42:44 +0100

    .

---
+++ UT2_Practica7/Script.ps1
@@ -0,0 +1,19 @@
+﻿$empleados = Import-Csv -Path C:\Users\Administrador\Desktop\archivos\empleados.csv -Delimiter ';'
+
+
+
+
+
+foreach ($emp in $empleados){
+
+$nombre = $emp.nombre
+$apellido = $emp.apellido
+$nomape = "$nombre.$apellido"
+$departament = $emp.departamento
+
+    if ($departament -eq "PERSONAL"){
+        Set-ADUser -Identity "$nomape" -ProfilePath "\\EMPRESA-DC1\Empresa_users$\$nomape" 
+        
+        
+    }
+}
